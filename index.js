3445463f9a24fb6830df358e09cf048aad1442ef
Alex Mendes <alexanderhmendes@gmail.com>
2023-01-10 22:46:33 +0000

    style: ignore rule

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Import the generated client from node_modules/.oac/index
 module.exports = {
-  // eslint-disable-next-line import/no-unresolved
+  // eslint-disable-next-line import/no-unresolved, import/no-useless-path-segments
   ...require('.oac/index'),
 };

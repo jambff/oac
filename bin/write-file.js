fff663125be2265154c3494486d0384c946689de
Alex Mendes <alexanderhmendes@gmail.com>
2023-01-10 22:47:55 +0000

    docs: remove a useless comment

--- bin/write-file.js
+++ bin/write-file.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const fse = require('fs-extra');
 
-/**
- * Write a file.
- */
 module.exports.writeFile = async (outPath, content) => {
   fse.ensureDirSync(path.dirname(outPath));
 
